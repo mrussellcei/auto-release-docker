b0873f025750e9c9b489a4db1668fd2baf115054
“Mark <mrussell@ceiamerica.com>
2024-12-04 11:15:50 -0500

    add a debug line to the entry script

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 set -euo pipefail
 
-echo "::set-output name=release-url::http://example.com
+echo "::debug::Seting the release URL"
+echo "::set-output name=release-url::http://example.com"
 
 exit 0
